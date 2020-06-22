a097f116dfd677406a578c04d11a74c3c43655c9
ryym <ryym.64@gmail.com>
2020-06-22 13:11:08 +0900

    feat: Add form validation for books/create

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -4,9 +4,22 @@ module Web
       class Create
         include Web::Action
 
+        expose :book
+
+        params do
+          required(:book).schema do
+            required(:title).filled(:str?)
+            required(:author).filled(:str?)
+          end
+        end
+
         def call(params)
-          BookRepository.new.create(params[:book])
-          redirect_to '/books'
+          if params.valid?
+            BookRepository.new.create(params[:book])
+            redirect_to '/books'
+          else
+            self.status = 422
+          end
         end
       end
     end
